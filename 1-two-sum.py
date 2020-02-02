640176d876518af4d12581357c4884874d0a5b51
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-02-02 16:22:17 +0800

    Create 1-two-sum.py

---
+++ 1-two-sum.py
@@ -0,0 +1,21 @@
+```
+Use recursion.
+
+Time Complexity: O(n^2)
+
+Can be improved:
+- Add Exeption if there's no match
+- Use Hash table to decrease time complexity
+```
+class Solution:
+    def __init__(self):
+        self._firstIndex = 0
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        secondIndex = self._firstIndex
+        for i in nums[1:]:
+            secondIndex = secondIndex + 1
+            if nums[0] + i == target:
+                return [self._firstIndex, secondIndex]
+        self._firstIndex = self._firstIndex + 1
+        return Solution.twoSum(self, nums[1:], target)
